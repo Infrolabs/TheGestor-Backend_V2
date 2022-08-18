a3b84620f02165c1a18683d1ded369e17f6c3471
nilkanth987 <parmarnilkanth@gmail.com>
2022-08-18 19:18:52 +0530

    Removed debugger for mongoose

--- src/app.ts
+++ src/app.ts
@@ -50,10 +50,6 @@ class App {
   }
 
   private connectToDatabase() {
-    if (this.env !== 'production') {
-      set('debug', true);
-    }
-
     connect(dbConnection.url, dbConnection.options as ConnectOptions);
   }
 
