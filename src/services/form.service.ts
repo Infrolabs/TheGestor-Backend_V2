d60ac96911e528e9aa420ad7ac373a641c3a08eb
nilkanth987 <parmarnilkanth@gmail.com>
2022-12-05 17:49:37 +0530

    Cell 2 update

--- src/services/form.service.ts
+++ src/services/form.service.ts
@@ -228,13 +228,14 @@ class FormService {
                     }
                 }
             ])
-            let cell2Total = 0
+            let prevCell2 = 0
             let cell7Total = 0
             let cell16Total = 0
             const taxes = await taxModel.find({ userId: userId, year, trimester: { $lt: trimester }, type: ETaxType.FORM130 })
             taxes.forEach(tax => {
                 if (tax.data && Object.keys(tax.data).length > 15) {
-                    cell2Total += Number(tax.data[1]) || 0
+                    if (tax.trimester === trimester - 1)
+                        prevCell2 = Number(tax.data[1]) || 0
                     if ((Number(tax.data[6]) || 0) > 0) {
                         cell7Total += Number(tax.data[6])
                         cell16Total += (Number(tax.data[15]) || 0)
@@ -246,7 +247,7 @@ class FormService {
             const totalIncome = cumulativeIncomes[0]?.totalTriIncome || 0
             const totalExpense = (expenses[0]?.totalExpense || 0)
             dataArray[0] = cumulativeIncomes[0]?.totalIncome || 0
-            dataArray[1] = ((totalIncome - totalExpense) * 5 / 100) + totalExpense + cell2Total
+            dataArray[1] = ((totalIncome - totalExpense) * 5 / 100) + totalExpense + prevCell2
             dataArray[4] = cell7Total - cell16Total
             const data = Object.fromEntries(dataArray.map((element, index) => [String(index), element]))
             data.isSimplified = "true"
